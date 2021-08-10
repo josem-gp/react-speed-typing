bb735fa5c904bbf48d212fc5670c6a7359562e74
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2021-08-11 06:57:09 +0900

    created App component with main elements

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,15 @@
-function App() {
-  return <div>hi</div>;
-}
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Title</h1>
+      <textarea />
+      <h4>Time remaining</h4>
+      <button>Start the game!</button>
+      <h1>Word Count</h1>
+    </div>
+  );
+};
 
 export default App;
