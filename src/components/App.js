a02afa95639ae9be6149381b83ce2e5e38ed28d0
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2021-08-11 07:59:43 +0900

    created useState to handle input in text area

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,20 @@
 import React from "react";
 
 const App = () => {
+  const [type, setType] = useState("");
+
+  const handleChange = (event) => {
+    const { value } = event.target;
+    setType(value);
+  };
+
   return (
     <div>
       <h1>How fast do you type?</h1>
       <textarea />
       <h4>Time remaining: ???</h4>
       <button>Start</button>
-      <h1>Word count: ??</h1>
+      <h1>Word count: ???</h1>
     </div>
   );
 };
