e8388b2056838b6dd1c45c7422dcece1f3e25aec
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2021-08-11 11:24:29 +0900

    added useEffect for countdown

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const App = () => {
   const [type, setType] = useState("");
@@ -13,6 +13,14 @@ const App = () => {
     const wordsArr = type.trim().split(" ");
     return wordsArr.filter((word) => word !== "").length;
   };
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      if (timeRemaining !== 0) {
+        setTimeRemaining(timeRemaining - 1);
+      }
+    }, 1000);
+  });
 
   return (
     <div>
